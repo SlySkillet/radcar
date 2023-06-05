8cb058b1f5fbfd99d4805962e0ee7d0ef512c831
mac stephens <macstephens@gmail.com>
2023-06-05 18:18:20 -0400

    finished the sales poller and all sales models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return self.last_name
+
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.last_name
+
+
+class Sale(models.Model):
+    price = models.PositiveBigIntegerField()
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sale",
+        on_delete=models.CASCADE
+    )
+
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sale",
+        on_delete=models.CASCADE
+    )
+
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sale",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.id
+    
+
