828019a55eb47f8fed4ce412a5bbcf070a8c24a0
Simon Conrad <simondbconrad@gmail.com>
2023-06-05 16:32:12 -0400

    create poller function for AutomobileVO data

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,6 +11,7 @@ django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
+from service_rest.models import AutomobileVO
 
 
 def poll(repeat=True):
@@ -19,8 +20,14 @@ def poll(repeat=True):
         try:
             # Write your polling logic, here
             # Do not copy entire file
-            pass
-        
+            response = requests.get("http://localhost:8100/api/automobiles/")
+            content = json.loads(response.content)
+            for automobile in content["automobiles"]:
+                AutomobileVO.objects.update_or_create(
+                    vin=automobile["vin"],
+                    defaults={"sold": automobile["sold"]}
+                )
+
         except Exception as e:
             print(e, file=sys.stderr)
 
