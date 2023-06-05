06d545f7b8279e8f6029c707c232f20e8e3fda99
Simon Conrad <simondbconrad@gmail.com>
2023-06-05 16:31:38 -0400

    migrate model changes

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AutomobileVO(models.Model):
-    vin = models.CharField(max_length=200)
+    vin = models.CharField(max_length=17)
     sold = models.BooleanField(default=False)
 
 class Technician(models.Model):
