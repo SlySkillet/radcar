d01d9d09a6f52f01cadeb2c018f80e48c575f571
mac stephens <macstephens@gmail.com>
2023-06-06 16:45:27 -0400

    working on modelform

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-import ManufacturersPage from './ManufacturersPage';
-import ManufacturersForm from './ManufacturersForm';
+// import ManufacturersPage from './ManufacturersPage';
+// import ManufacturersForm from './ManufacturersForm';
 import ModelsPage from './ModelsPage'
 import ModelsForm from './ModelsForm'
-import AutomobilesPage from './AutomobilesPage'
-import AutomobilesForm from './AutomobilesForm'
+// import AutomobilesPage from './AutomobilesPage'
+// import AutomobilesForm from './AutomobilesForm'
 
 function App() {
   return (
@@ -15,12 +15,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/manufacturers" element={<ManufacturersPage />} />
-          <Route path="/manufacturers/create" element={<ManufacturersForm />} />
+          {/* <Route path="/manufacturers" element={<ManufacturersPage />} />
+          <Route path="/manufacturers/create" element={<ManufacturersForm />} /> */}
           <Route path="/models" element={<ModelsPage />} />
           <Route path="/models/create" element={<ModelsForm />} />
-          <Route path="/automobiles" element={<AutomobilesPage />} />
-          <Route path="/automobiles/create" element={<AutomobilesForm />} />
+          {/* <Route path="/automobiles" element={<AutomobilesPage />} />
+          <Route path="/automobiles/create" element={<AutomobilesForm />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/ModelsPage.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+function ModelsPage() {
+    const [models, setModels] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = 'http://localhost:8100/api/models/';
+            const response = await fetch(url);
+
+            if (response.ok) {
+                const data = await response.json();
+                setModels(data.models);
+            }
+
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="table-responsive">
+            <h2 className="text-center">Vehicle Models</h2>
+                <table className="table-fill table table-shadow table-striped">
+                    <thead>
+                        <tr>
+                            <th className="text-left">Model</th>
+                            <th className="text-left">Manufacturer</th>
+                            <th className="text-left">Picture</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-hover">
+                        {models.map(model => {
+                            return (
+                                <tr key={model.id}>
+                                    <td className="text-left w-25">{model.name}</td>
+                                    <td className="text-left w-25">{model.manufacturer.name}</td>
+                                    <td className="text-center"><img className="img-fluid w-75" src={model.picture_url} alt="preview" /></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+        </div>        
+    )
+}
+
+export default ModelsPage
