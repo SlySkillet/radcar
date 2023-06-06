25c941b23ea4c9b7ca0896c1ffe2882fa3c3254d
Simon Conrad <simondbconrad@gmail.com>
2023-06-06 13:01:17 -0400

    create inventory pages and routes

---
+++ ghi/app/src/ManufacturersPage.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+import { NavLink } from 'react-router-dom'
+
+function ManufacturersPage(props){
+    return (
+        <p>manufacturers page</p>
+    )
+}
+
+export default ManufacturersPage
