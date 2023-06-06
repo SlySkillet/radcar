85e3b231f21aea6980d299e17ef979df427c23d0
Simon Conrad <simondbconrad@gmail.com>
2023-06-06 17:49:42 -0400

    create automobiles table

--- ghi/app/src/AutomobilesPage.js
+++ ghi/app/src/AutomobilesPage.js
@@ -3,25 +3,48 @@ import React, {useState, useEffect} from 'react'
 
 
 function AutomobilesPage(props){
+    const [automobiles, setAutomobiles] = useState([])
+    useEffect(() => {
+        async function loadAutomobiles() {
+            const response = await fetch('http://localhost:8100/api/automobiles/');
+            if (response.ok){
+                const data = await response.json();
+                console.log("data ==>", data)
+                setAutomobiles(data.autos)
+                console.log("automobiles ==>", automobiles)
+            } else {
+                console.error(response)
+            }
+        }
+        loadAutomobiles()
+    }, [])
     return(
         <div className="px-4 py-5 my-5 text-center">
         <h1 className="display-5 fw-bold">Automobiles</h1>
         <table className="table table-striped">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Picture</th>
+                    <th>VIN</th>
+                    <th>Color</th>
+                    <th>Year</th>
                     <th>Manufacturer</th>
+                    <th>Model</th>
+                    <th>Sold</th>
                 </tr>
             </thead>
             <tbody>
-                {/* {manufacturers.map(manufacturer => {
+                {automobiles.map(automobile => {
                     return(
-                        <tr key={manufacturer.id}>
-                            <td>{manufacturer.name}</td>
+                        <tr key={automobile.id}>
+                            <td>{automobile.vin}</td>
+                            <td>{automobile.color}</td>
+                            <td>{automobile.year}</td>
+                            <td>{automobile.model.manufacturer.name}</td>
+                            <td>{automobile.model.name}</td>
+                            <td>{automobile.sold}</td>
                         </tr>
                     )
-                })} */}
+                })}
             </tbody>
         </table>
     </div>
