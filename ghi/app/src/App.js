5a39651dd84cc1ff4f50028c083a6d15f29a0a4f
Simon Conrad <simondbconrad@gmail.com>
2023-06-06 14:56:58 -0400

    create table for manufacturers

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,10 +3,10 @@ import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturersPage from './ManufacturersPage';
 import ManufacturersForm from './ManufacturersForm';
-import ModelsPage from './ModelsPage'
-import ModelsForm from './ModelsForm'
-import AutomobilesPage from './AutomobilesPage'
-import AutomobilesForm from './AutomobilesForm'
+// import ModelsPage from './ModelsPage'
+// import ModelsForm from './ModelsForm'
+// import AutomobilesPage from './AutomobilesPage'
+// import AutomobilesForm from './AutomobilesForm'
 
 function App() {
   return (
@@ -17,10 +17,10 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/manufacturers" element={<ManufacturersPage />} />
           <Route path="/manufacturers/create" element={<ManufacturersForm />} />
-          <Route path="/models" element={<ModelsPage />} />
+          {/* <Route path="/models" element={<ModelsPage />} />
           <Route path="/models/create" element={<ModelsForm />} />
           <Route path="/automobiles" element={<AutomobilesPage />} />
-          <Route path="/automobiles/create" element={<AutomobilesForm />} />
+          <Route path="/automobiles/create" element={<AutomobilesForm />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/ManufacturersPage.js
+++ ghi/app/src/ManufacturersPage.js
@@ -1,9 +1,42 @@
 import React, {useState, useEffect} from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 function ManufacturersPage(props){
+    const [manufacturers, setManufacturers] = useState([]);
+    useEffect(() => {
+        async function loadManufacturers() {
+            const response = await fetch('http://localhost:8100/api/manufacturers/');
+            if (response.ok){
+                const data = await response.json();
+                // console.log("data ==>", data)
+                // console.log("data.manufacturers ==>", data.manufacturers)
+                setManufacturers(data.manufacturers);
+                console.log("manufacturers ==>", manufacturers)
+            }
+        }
+        loadManufacturers()
+        console.log(manufacturers)
+    }, [])
     return (
-        <p>manufacturers page</p>
+        <div className="px-4 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold">Manufacturers</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {manufacturers.map(manufacturer => {
+                        return(
+                            <tr key={manufacturer.id}>
+                                <td>{manufacturer.name}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
