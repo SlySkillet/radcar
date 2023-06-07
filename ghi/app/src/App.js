64d969ce5a279c963938d652eb55860a670f3e82
Simon Conrad <simondbconrad@gmail.com>
2023-06-07 15:52:48 -0400

    create update status buttons

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-// import ManufacturersPage from './ManufacturersPage';
-// import ManufacturersForm from './ManufacturersForm';
+import ManufacturersPage from './ManufacturersPage';
+import ManufacturersForm from './ManufacturersForm';
 // import ModelsPage from './ModelsPage'
 // import ModelsForm from './ModelsForm'
 // import AutomobilesPage from './AutomobilesPage'
 // import AutomobilesForm from './AutomobilesForm'
 
-// import TechniciansPage from './services/TechniciansPage';
-// import TechniciansForm from './services/TechnicansForm';
-// import AppointmentsPage from './services/AppointmentsPage';
+import TechniciansPage from './services/TechniciansPage';
+import TechniciansForm from './services/TechnicansForm';
+import AppointmentsPage from './services/AppointmentsPage';
 import AppointmentsForm from './services/AppointmentsForm';
 
 function App() {
@@ -19,16 +19,16 @@ function App() {
       <Nav />
       <div className="container">
         <Routes>
-          {/* <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/manufacturers" element={<ManufacturersPage />} />
           <Route path="/manufacturers/create" element={<ManufacturersForm />} />
-          <Route path="/models" element={<ModelsPage />} />
-          <Route path="/models/create" element={<ModelsForm />} />
-          <Route path="/automobiles" element={<AutomobilesPage />} />
-          <Route path="/automobiles/create" element={<AutomobilesForm />} />
+          {/* <Route path="/models" element={<ModelsPage />} /> */}
+          {/* <Route path="/models/create" element={<ModelsForm />} /> */}
+          {/* <Route path="/automobiles" element={<AutomobilesPage />} /> */}
+          {/* <Route path="/automobiles/create" element={<AutomobilesForm />} /> */}
           <Route path="/technicians" element={<TechniciansPage />} />
           <Route path="/technicians/create" element={<TechniciansForm />} />
-          <Route path="/appointments" element={<AppointmentsPage />} /> */}
+          <Route path="/appointments" element={<AppointmentsPage />} />
           <Route path="/appointments/create" element={<AppointmentsForm />} />
         </Routes>
       </div>
--- ghi/app/src/services/AppointmentsPage.js
+++ ghi/app/src/services/AppointmentsPage.js
@@ -8,13 +8,46 @@ function AppointmentsPage(props){
             const response = await fetch ('http://localhost:8080/api/appointments/');
             if (response.ok){
                 const data = await response.json();
-                console.log(data)
-                setAppointments(data.appointments)
+                const displayData = data.appointments.filter((appointment) => appointment.status === "scheduled")
+                setAppointments(displayData)
             }
         }
         loadAppointments();
-        console.log(appointments)
     }, [])
+
+    const handleCancel = async (id, event) => {
+        const cancelUrl = `http://localhost:8080/api/appointments/${id}/cancel`
+        const fetchConfig = {
+            method: "put",
+        };
+        const response = await fetch(cancelUrl, fetchConfig);
+        if (response.ok) {
+            const updateStatus = await response.json();
+            console.log(updateStatus);
+        //     console.log("appointments =>", appointments)
+
+        //     const updatedData = appointments.filter((appointment) => appointment.status === "scheduled");
+        //     console.log("updatedData =>", updatedData)
+        //     setAppointments(updatedData);
+        }
+    }
+
+    const handleFinish = async (id, event) => {
+        console.log("id ==>", id)
+        const finishUrl = `http://localhost:8080/api/appointments/${id}/finish`
+        const fetchConfig = {
+            method: "put",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+        const response = await fetch(finishUrl, fetchConfig);
+        const updateStatus = await response.json();
+        console.log(updateStatus);
+        const updatedData = appointments.filter((appointment) => appointment.status === "scheduled");
+        setAppointments(updatedData);
+    }
+
     return(
         <div className="px-4 py-5 my-5 text-center">
             <h1 className="display-5 fw-bold">Appointments</h1>
@@ -26,10 +59,12 @@ function AppointmentsPage(props){
                         <th>Date</th>
                         <th>Technician</th>
                         <th>Reason</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {appointments.map(appointment => {
+
                         return(
                             <tr key={appointment.id}>
                                 <td>{appointment.vin}</td>
@@ -37,6 +72,10 @@ function AppointmentsPage(props){
                                 <td>{new Date(appointment.date_time).toLocaleString()}</td>
                                 <td>{appointment.technician.first_name} {appointment.technician.last_name}</td>
                                 <td>{appointment.reason}</td>
+                                <td>
+                                    <button onClick={(event) => handleCancel(appointment.id, event)} className="btn btn-warning mx-3" >Cancel</button>
+                                    <button onClick={(event) => handleFinish(appointment.id, event)} className="btn btn-primary">Finish</button>
+                                </td>
                             </tr>
                         )
                     })
