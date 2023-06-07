45c435d2618bab05accbe04245cfc466ca50b456
Simon Conrad <simondbconrad@gmail.com>
2023-06-07 19:02:07 -0400

    create filter functionality on form submit

--- ghi/app/src/services/ServiceHistory.js
+++ ghi/app/src/services/ServiceHistory.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react'
 
 function ServiceHistory(props){
     const [appointments, setAppointments] = useState([]);
+    const [ filterVin, setFilterVin] = useState('');
     useEffect(() => {
         async function loadAppointments() {
             const response = await fetch ("http://localhost:8080/api/appointments/")
@@ -15,12 +16,27 @@ function ServiceHistory(props){
         }
         loadAppointments()
     }, [])
+    const handleFilterVinChange = (event) => {
+        const value = event.target.value;
+        setFilterVin(value)
+    }
+
+    const filterAppointments = async (event) => {
+        event.preventDefault();
+
+
+        const filteredData = appointments.filter(appointment => appointment.vin === filterVin)
+        console.log(filteredData)
+
+        setAppointments(filteredData)
+    }
+
     return(
 
         <div className="px-4 py-5 my-5 text-center">
             <h1 className="display-5 fw-bold">Service History</h1>
-            <form className="form-inline">
-                <input placeholder="search by automobile vin" required type="text" name="vin" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2"/>
+            <form onSubmit={filterAppointments} className="form-inline">
+                <input value={filterVin} onChange={handleFilterVinChange} placeholder="search by automobile vin" required type="text" name="vin" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2"/>
                 <button type="submit" className="btn btn-light">search</button>
             </form>
             <table className="table table-striped">
