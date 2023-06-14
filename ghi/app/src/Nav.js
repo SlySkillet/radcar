28febbdb9b7cb1b5777a53cd79f548fd6b85f94d
Simon Conrad <simondbconrad@gmail.com>
2023-06-14 17:55:12 -0400

    restore pages after style experiment

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,13 +1,10 @@
 import { NavLink } from 'react-router-dom';
-        <div className="dropdown">
-          <button>
-            Sales
-          </button>
-        </div>
+
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+    // bg-success p-2 text-dark bg-opacity-25
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary" >
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -100,68 +97,6 @@ function Nav() {
             </div>
           </ul>
         </div>
-
-        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="manufacturers/">Manufacturers</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="manufacturers/create">Add Manufacturer</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="models/">Models</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="models/create">Add Model</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="automobiles/">Automobiles</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="automobiles/create">Add Automobile</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="sales/">Sales</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="sales/create">Add Sale</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="salespeople">Salespeople</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="salespeople/create">Add Salesperson</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="customers/">Customers</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="customers/create">Add Customer</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="salespeople/history">Sales History</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="technicians/">Technicians</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="technicians/create">Add Technician</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="appointments">Appointments</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="appointments/create">Add Appointment</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="appointments/history">Appointment History</NavLink>
-            </li>
-          </ul>
-        </div> */}
       </div>
     </nav>
   )
