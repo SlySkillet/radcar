745550c5103deafe8d208385afb7a4f3c35e1a37
Simon Conrad <simondbconrad@gmail.com>
2023-06-15 18:27:51 -0400

    finish initial styles for main page

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,14 +9,15 @@ function Nav() {
     // className="navbar navbar-expand-lg navbar-dark bg-secondary"
     <nav id="bootstrap-overrides"  className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">RadCar < FaBiohazard /></NavLink>
+          <NavLink className="navbar-brand" to="/">RadCar < FaBiohazard /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <div> </div>
-            <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+            {/* <div>
+              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+            </div> */}
             <div className="dropdown">
               <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Sales
