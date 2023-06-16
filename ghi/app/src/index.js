4eb22500fedc1ad009ff5b7af04f962d920d4bbd
Simon Conrad <simondbconrad@gmail.com>
2023-06-16 14:20:41 -0400

    style forms

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './css/index.css'
-import './css/table-pages.css'
+import './css/index.css';
+import './css/table-pages.css';
+import './css/form-pages.css';
 
 
 
