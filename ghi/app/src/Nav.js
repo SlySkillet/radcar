f6c92f99ca03273e861e56f3cbb4ae6dd3ee30ae
Simon Conrad <simondbconrad@gmail.com>
2023-06-24 13:06:11 -0400

    fix floating footer issue

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -11,7 +11,7 @@ function Nav() {
       <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">RadCar < FaBiohazard /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon">< FaHamburger /></span>
+          <span className="navbar-toggler-icon"><FaHamburger /></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
